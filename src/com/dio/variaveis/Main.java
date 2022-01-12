12fc92e4acc1a77797843e4767f676052fa5a543
Cuckoo-Head <cuckoohead@disroot.org>
2022-01-12 13:13:23 -0300

    commit inicial, variaveis de varios tipos criadas

---
+++ src/com/dio/variaveis/Main.java
@@ -0,0 +1,29 @@
+package com.dio.variaveis;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		byte b = 120;
+		short s = 320;
+		int i = 4700;
+		long l = 550092234;
+		float f = 534.23f;
+		double d = 23948.8329483289;
+		char c = 'T';
+		String S = "String";
+		boolean boo = false;
+		
+		System.out.println("This is byte: " + b);
+		System.out.println("This is short: " + s);
+		System.out.println("This is int: " + i);
+		System.out.println("This is long: " + l);
+		System.out.println("This is float: " + f);
+		System.out.println("This is double: " + d);
+		System.out.println("This is char: " + c);
+		System.out.println("This is string: " + S);
+		System.out.println("This is boolean: " + boo);
+	}
+
+}
